f15b1dd33bcfbd21cf15ca0497a6c9937be0ba54
Askany83 <askany.andre@gmail.com>
2024-11-21 17:25:57 +0000

    mongoDB on!

--- app/api/dbTest/route.tsx
+++ app/api/dbTest/route.tsx
@@ -1,5 +1,23 @@
 import { connectMongoDB } from "@/dbConnet";
+import { NextResponse } from "next/server";
 
-export const test = async () => {
-  await connectMongoDB();
+// Named export for GET method
+export const GET = async () => {
+  try {
+    await connectMongoDB(); // Establish connection to MongoDB
+    return new NextResponse(
+      JSON.stringify({ message: "Successfully connected to MongoDB" }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    return new NextResponse("Failed to connect to MongoDB: " + error, {
+      status: 500,
+    });
+  }
 };
